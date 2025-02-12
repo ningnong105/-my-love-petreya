99c548026ab1802c8981ba52b2d36093f4ee2489
ningnong105 <kevinnguyen60640@gmail.com>
2025-02-12 21:57:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -75,7 +75,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "", // Music streaming URL
+        musicUrl: "https://www.youtube.com/watch?v=GPGdXrQID7c", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
