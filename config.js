7bc776c10814fe1474c67c44be8ac2c38007b7e8
ningnong105 <kevinnguyen60640@gmail.com>
2025-02-12 21:42:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
-// ============================================
-// 💝 CUSTOMIZE YOUR VALENTINE'S WEBSITE HERE 💝
-// ============================================
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Petreya!",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -50,7 +47,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "MWAHHHHHH I LOVEEE YOUUU SOOOO MUCH PETREYAAA💖🤗💝💋💋!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
